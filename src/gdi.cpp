7f7614ffa924b503b628f962792694319448eab0
Szitár Gergő <szitar.gergo@gmail.com>
2021-09-25 00:05:40 +0200

    dirty hack

--- src/gdi.cpp
+++ src/gdi.cpp
@@ -5,6 +5,8 @@
 #include <windows.h>
 #include <gdiplus.h>
 
+#include <numeric>
+
 static ULONG_PTR gdip_token = 0;
 
 void init_gdi()
@@ -33,6 +35,21 @@ static inline uint32_t rgb_to_bgr(uint32_t rgb)
 	return ((rgb & 0xFF) << 16) | (rgb & 0xFF00) | ((rgb & 0xFF0000) >> 16);
 }
 
+std::vector<std::wstring> split_string(const std::wstring& str, const std::wstring& delimiter)
+{
+    std::vector<std::wstring> strings;
+
+    std::wstring::size_type prev = 0;
+	for (std::wstring::size_type pos = str.find(delimiter, prev); pos != std::string::npos; pos = str.find(delimiter, prev))
+    {
+        strings.push_back(str.substr(prev, pos - prev));
+        prev = pos + delimiter.size();
+    }
+
+    strings.push_back(str.substr(prev));
+    return strings;
+}
+
 std::vector<uint8_t> render_text(uint32_t width, uint32_t height, const std::wstring& text, uint32_t color, int i)
 {
 	std::vector<uint8_t> bits(width * height * 4, 0);
@@ -42,6 +59,7 @@ std::vector<uint8_t> render_text(uint32_t width, uint32_t height, const std::wst
 	Gdiplus::FontFamily x(L"Arial");
 	Gdiplus::Font font(&x, 72);
 	Gdiplus::SolidBrush brush = Gdiplus::Color(calc_color(rgb_to_bgr(color), 100));
+
 	if (Gdiplus::Status st = gr.Clear(Gdiplus::Color(0)); st == Gdiplus::Status::Ok)
 	{
 		Gdiplus::StringFormat format(Gdiplus::StringFormat::GenericTypographic());
@@ -49,24 +67,71 @@ std::vector<uint8_t> render_text(uint32_t width, uint32_t height, const std::wst
 		format.SetLineAlignment(Gdiplus::StringAlignment::StringAlignmentCenter);
 
 		Gdiplus::RectF before;
-		if (Gdiplus::Status st = gr.MeasureString(text.c_str(), -1, &font, Gdiplus::PointF(0, 0), &format, &before); st == Gdiplus::Status::Ok && before.Width != 0 && before.Height != 0)
+		if (Gdiplus::Status st = gr.MeasureString(text.c_str(), static_cast<int>(text.size()), &font, Gdiplus::PointF(0, 0), &format, &before); st == Gdiplus::Status::Ok && before.Width != 0 && before.Height != 0)
 		{
-			float scale = min(float(width) / before.Width, float(height) / before.Height);
+			float width_scale = float(width) / before.Width;
+			float height_scale = float(height) / before.Height;
 
-			if (Gdiplus::Status st = gr.ScaleTransform(scale, scale); st == Gdiplus::Status::Ok)
+			if ((height_scale * 2) < width_scale && text.find(L'\n') != std::wstring::npos)
 			{
-				if (Gdiplus::Status st = gr.DrawString(text.c_str(), -1, &font, Gdiplus::PointF((i == 0 ? 0 : width) / scale, (height / 2) / scale), &format, &brush); st == Gdiplus::Status::Ok)
+				std::vector<std::wstring> lines = split_string(text, L"\n");
+
+				std::wstring leftPart = std::accumulate(lines.begin() + 1, lines.begin() + (lines.size() / 2), lines.front(), [](std::wstring s0, const std::wstring& s1) { return std::move(s0.append(L"\n").append(s1)); });
+				std::wstring rightPart = std::accumulate(lines.begin() + 1 + (lines.size() / 2), lines.end(), *(lines.begin() + (lines.size() / 2)), [](std::wstring s0, const std::wstring& s1) { return std::move(s0.append(L"\n").append(s1)); });
+
+				Gdiplus::RectF beforeLeft;
+				if (Gdiplus::Status st = gr.MeasureString(leftPart.c_str(), static_cast<int>(leftPart.size()), &font, Gdiplus::PointF(0, 0), &format, &beforeLeft); st == Gdiplus::Status::Ok && beforeLeft.Width != 0 && beforeLeft.Height != 0)
 				{
-					return bits;
-				}
-				else
-				{
-					blog(LOG_WARNING, "DrawString failed, with text: %s", text.c_str());
+					Gdiplus::RectF beforeRight;
+					if (Gdiplus::Status st = gr.MeasureString(rightPart.c_str(), static_cast<int>(rightPart.size()), &font, Gdiplus::PointF(0, 0), &format, &beforeRight); st == Gdiplus::Status::Ok && beforeRight.Width != 0 && beforeRight.Height != 0)
+					{
+						float double_width_scale = min(float(width / 2) / beforeLeft.Width, float(width / 2) / beforeRight.Width);
+						float double_height_scale = min(float(height) / beforeLeft.Height, float(height) / beforeRight.Height);
+						float double_scale = min(double_width_scale, double_height_scale);
+
+						if (Gdiplus::Status st = gr.ScaleTransform(double_scale, double_scale); st == Gdiplus::Status::Ok)
+						{
+							if (Gdiplus::Status st = gr.DrawString(leftPart.c_str(), static_cast<int>(leftPart.size()), &font, Gdiplus::PointF((i == 0 ? 0 : width) / double_scale, (height / 2) / double_scale), &format, &brush); st == Gdiplus::Status::Ok)
+							{
+								if (Gdiplus::Status st = gr.DrawString(rightPart.c_str(), static_cast<int>(rightPart.size()), &font, Gdiplus::PointF((width / 2) / double_scale, (height / 2) / double_scale), &format, &brush); st == Gdiplus::Status::Ok)
+								{
+									return bits;
+								}
+								else
+								{
+									blog(LOG_WARNING, "DrawString failed, with text: %s", rightPart.c_str());
+								}
+							}
+							else
+							{
+								blog(LOG_WARNING, "DrawString failed, with text: %s", leftPart.c_str());
+							}
+						}
+						else
+						{
+							blog(LOG_WARNING, "ScaleTransform failed, with scale: %f", double_scale);
+						}
+					}
 				}
 			}
 			else
 			{
-				blog(LOG_WARNING, "ScaleTransform failed, with scale: %f", scale);
+				float scale = min(width_scale, height_scale);
+				if (Gdiplus::Status st = gr.ScaleTransform(scale, scale); st == Gdiplus::Status::Ok)
+				{
+					if (Gdiplus::Status st = gr.DrawString(text.c_str(), static_cast<int>(text.size()), &font, Gdiplus::PointF((i == 0 ? 0 : width) / scale, (height / 2) / scale), &format, &brush); st == Gdiplus::Status::Ok)
+					{
+						return bits;
+					}
+					else
+					{
+						blog(LOG_WARNING, "DrawString failed, with text: %s", text.c_str());
+					}
+				}
+				else
+				{
+					blog(LOG_WARNING, "ScaleTransform failed, with scale: %f", scale);
+				}
 			}
 		}
 		else
